f3c1c32173453abacb0a1a5d92939380e955d6d5
Zoe Henderson <zzhenders@gmail.com>
2019-10-28 16:05:02 -0700

    CLI added.

--- oxcalc.py
+++ oxcalc.py
@@ -1,3 +1,45 @@
-import argparse
-from functions import dec_to_bin, dec_to_hex
-from functions import hex_to_bin, hex_to_dec, hex_to_ch
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from stack import Stack
+
+parser = ArgumentParser(
+    formatter_class=RawDescriptionHelpFormatter,
+    usage="""oxcalc [-h] [-b BASE] <expression>""",
+    description="""Simple command-line programmer's calculator.""",
+    epilog="""
+bases:
+  he[x]adecimal, [d]ecimal, [b]inary, [c]haracters
+
+arithmetic operators:
+  a b +
+  a b -
+  a b *
+  a b /   integer division
+  a b %   a modulo b
+        
+logical operators:
+  a b &   and
+  a b |   or
+  a ~     not
+  a b ^   xor
+  a b <   bitshift a left by b
+  a b >   bitshift a right by b
+
+note: you may need to escape operators to avoid shell conflicts.
+  """
+)
+
+parser.add_argument(
+    "-b", "--base",
+    help="set output base [default: x]",
+    default="x",
+    action="store",
+    dest="base",
+)
+
+parser.add_argument("expression", action="store", help="RPN expression", nargs="*")
+
+args = parser.parse_args()
+
+rpn_stack = Stack()
+answer = rpn_stack.parse(args.expression, args.base)
+print(answer)
